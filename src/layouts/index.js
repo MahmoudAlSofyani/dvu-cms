acfdcd233c74be3cfd76ce74bcbf4001bc2e27bb
Mahmoud AlSofyani <msafar95@hotmail.com>
2022-01-21 16:06:05 +0400

    added session to the layout component for global access

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,12 @@
 import { Container } from "@mui/material";
 import CustomNavDrawer from "../components/custom-nav-drawer";
 
-const Layout = ({ children, pageTitle }) => {
+const Layout = ({ children, pageTitle, session }) => {
   return (
     <Container maxWidth="xl">
-      <CustomNavDrawer pageTitle={pageTitle}>{children}</CustomNavDrawer>
+      <CustomNavDrawer pageTitle={pageTitle} session={session}>
+        {children}
+      </CustomNavDrawer>
     </Container>
   );
 };
